c31964f41b0f6b5b8b255d8eaaa012a87ccb8a22
Andre Cabalo <andreccabalo@gmail.com>
2024-08-04 11:46:00 -0300

    Ajustando nome Repo

--- src/main/java/br/com/andrecabalo/exception/ErroConsultaGitHubException.java
+++ src/main/java/br/com/andrecabalo/exception/ErroConsultaGitHubException.java
@@ -1,2 +1,16 @@
-package br.com.andrecabalo.exception;public class ErroConsultaGitHubException {
+package br.com.andrecabalo.exception;
+
+public class ErroConsultaGitHubException extends RuntimeException{
+
+    private String mensagem;
+
+    public ErroConsultaGitHubException(String mensagem){
+        this.mensagem = mensagem;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.mensagem;
+    }
+
 }
